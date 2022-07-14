2fea1f2abd4a564e2c0a7ff367f51609e7298cc2
lrbn86 <lrbn86@gmail.com>
2022-07-13 22:31:42 -0400

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'tdlists/index'
-  get 'tdlists/create'
-  get 'tdlists/update'
-  get 'tdlists/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '/api/v1' do
+    resources :tdlists
+  end
 end
